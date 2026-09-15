488888f1382f552989ff4868f24eb136b37cdbc4
Yerikken <aidana.erkengazina@narxoz.kz>
2026-09-15 12:50:30 +0500

    hw2task1

---
+++ hw2.dart
@@ -0,0 +1,5 @@
+// Homework 2: Safe ATM Banking Terminal Simulator
+
+// 1. checkBalance
+void checkBalance({required String name, required double balance}) =>
+    print("Текущий баланс $name: $balance");
