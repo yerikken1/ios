3bf47fe7cd31bab2984305fbfad1e841e0a4544d
Yerikken <aidana.erkengazina@narxoz.kz>
2026-09-15 12:51:56 +0500

    hw2task2

--- hw2.dart
+++ hw2.dart
@@ -3,3 +3,12 @@
 // 1. checkBalance
 void checkBalance({required String name, required double balance}) =>
     print("Текущий баланс $name: $balance");
+
+
+// 2. deposit
+double deposit({required double currentBalance, double? amount}) {
+  double depositAmount = amount ?? 0.0;
+  double updatedBalance = currentBalance + depositAmount;
+  print("Пополнение: $depositAmount. Новый баланс: $updatedBalance");
+  return updatedBalance;
+}
