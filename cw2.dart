8c88724130182bdf02d01f68f17868a61f516958
Yerikken <aidana.erkengazina@narxoz.kz>
2026-09-18 14:57:41 +0500

    cw2task1

---
+++ cw2.dart
@@ -0,0 +1,9 @@
+// 1.Create class Book
+class Book {
+  String title;
+  String author;
+  double price;
+  bool isBorrowed;
+
+  Book(this.title, this.author, this.price, {this.isBorrowed = false});
+}
