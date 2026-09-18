e56000c29e70fde4a162ff7b64a63851e2c43f5f
Yerikken <aidana.erkengazina@narxoz.kz>
2026-09-18 14:59:13 +0500

    cw2task2

--- cw2.dart
+++ cw2.dart
@@ -7,3 +7,24 @@ class Book {
 
   Book(this.title, this.author, this.price, {this.isBorrowed = false});
 }
+
+
+// 2.Create class Library
+class Library {
+  List<Book> _books = [];
+
+  // Add a book to the library
+  void addBook(Book book) {
+    _books.add(book);
+  }
+
+  // Get available books using .where()
+  List<Book> getAvailableBooks() {
+    return _books.where((book) => !book.isBorrowed).toList();
+  }
+
+  // Calculate total value using .fold()
+  double getTotalValue() {
+    return _books.fold(0, (sum, book) => sum + book.price);
+  }
+}
