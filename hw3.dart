2f18b11f1b58ed1d1ed5ecdc8ccae0191e2931fe
Yerikken <aidana.erkengazina@narxoz.kz>
2026-09-18 15:31:43 +0500

    hw3task1

---
+++ hw3.dart
@@ -0,0 +1,14 @@
+// 1.Abstract Class MediaItem
+abstract class MediaItem {
+  final String id;
+  final String title;
+  final double price;
+
+  MediaItem({
+    required this.id,
+    required this.title,
+    required this.price,
+  });
+
+  String getDetails();
+}
